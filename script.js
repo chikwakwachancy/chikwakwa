a65304146ac78674e94aa986dae908236ca9e2f0
chikwakwachancy <chikwakwachancy1@gmail.com>
2026-06-21 15:10:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-function calculate() {
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
+function calculateGPA() {
 
-    num1 = Number(num1);
-    num2 = Number(num2);
+    let c1 = Number(document.getElementById("course1").value);
+    let c2 = Number(document.getElementById("course2").value);
+    let c3 = Number(document.getElementById("course3").value);
+    let c4 = Number(document.getElementById("course4").value);
+    let c5 = Number(document.getElementById("course5").value);
+    let c6 = Number(document.getElementById("course6").value);
 
-    let sum = num1 + num2;
+    let average = (c1 + c2 + c3 + c4 + c5 + c6) / 6;
 
-    document.getElementById("result").innerText = "Result: " + sum;
+    let gpa;
+
+    if (average >= 80) {
+        gpa = 4.0;
+    }
+    else if (average >= 70) {s
+        gpa = 3.5;
+    }
+    else if (average >= 60) {
+        gpa = 3.0;
+    }
+    else if (average >= 50) {
+        gpa = 2.0;
+    }
+    else {
+        gpa = 0.0;
+    }
+
+    document.getElementById("result").innerHTML =
+        "Average: " + average.toFixed(2) +
+        "<br> GPA: " + gpa;
